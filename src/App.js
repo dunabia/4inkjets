a01c26826c416734c652d1ed21684462be03758d
dunabia <161461530+dunabia@users.noreply.github.com>
2025-04-14 13:54:34 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   let InstanceId_QA = "";
   let InstanceId_SEO = "1084458";
   let InstanceId_Carousel = "1084459";
-  let InstanceId_StarRating = "1074980";
+  let InstanceId_StarRating = "";
   let InstanceId_PromotedProd = "";
   let InstanceId_UgcGallery = "";
   let InstanceId_ReviewsTab = "";
